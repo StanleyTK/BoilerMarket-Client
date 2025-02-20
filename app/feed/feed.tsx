e4f7eec0bfb0b5f6bef3447dc77d81f7b24eb33d
stanleytk <kim4111@purdue.edu>
2025-02-20 02:17:45 -0500

    Edit profile feature with UI updates

--- app/feed/feed.tsx
+++ app/feed/feed.tsx
@@ -3,20 +3,18 @@ import { useAuthState } from 'react-firebase-hooks/auth';
 import { getAuth } from 'firebase/auth';
 import Listings from './components/Listings';
 
-const Home: React.FC = () => {
+const Feed: React.FC = () => {
   const auth = getAuth();
   const [user] = useAuthState(auth);
 
   return (
     <div className="bg-gray-800 min-h-screen text-white">
       <main className="p-8 text-center">
-        <h2 className="text-2xl font-bold mb-4">
-          Today's Picks
-        </h2>
+
         <Listings />
       </main>
     </div>
   );
 };
 
-export default Home;
+export default Feed;
--- app/routes.ts
+++ app/routes.ts
@@ -2,10 +2,11 @@ import { type RouteConfig, index, layout, route } from "@react-router/dev/routes
 
 export default [
   layout("./components/MainLayout.tsx", [
-    index("home/home.tsx"),
+    index("feed/feed.tsx"),
     route("login", "./login/login.tsx"),
     route("register", "./register/register.tsx"),
     route("settings", "./settings/settings.tsx"),
     route("u/:uid", "./user/[uid].tsx"),
+    route("u/:uid/edit", "./edit/edit.tsx"),
   ]),
 ] satisfies RouteConfig;
