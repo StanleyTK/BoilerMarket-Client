61a2d234d5c3ac0f76dc6e7898b07914af35db45
Daniel <dxgeva@gmail.com>
2025-02-25 20:46:55 -0500

    Made changes to allow listings to show up on search page

--- app/search/search.tsx
+++ app/search/search.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import { useSearch } from "../components/MainLayout";
+import { ListingCard } from "~/components/ListingCard";
 import './search.css';
 
 const Search: React.FC = () => {
-
+    const { listings } = useSearch();
 
     return (
         <div className="search-container">
-
+            {listings.length > 0 ? (
+                <div className="grid gap-6 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 p-6">
+                    {listings.map((listing, index) => (
+                        <ListingCard key={index} listing={listing} />
+                    ))}
+                </div>
+            ) : (
+                <p>No listings found.</p>
+            )}
         </div>
     );
 };
