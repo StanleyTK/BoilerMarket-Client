f65d12ada8fd2949151eba10308f42355f3bbfe4
Andrew Bradley <andrewcbradley007@gmail.com>
2025-02-26 20:08:37 -0500

    Get listing by user completed

---
+++ app/components/UserListings.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+import { ListingCard } from './ListingCard';
+import { fetchListingByUser } from '~/service/fetch-listings';
+
+interface Listing {
+  id: number;
+  title: string;
+  description: string;
+  price: number | string;
+  image?: string;
+  displayName?: string;
+  uid: string;
+}
+
