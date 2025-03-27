bd719ca2150dbcd961c389d3f6a69146cac559aa
Varun Jasti <varunjasti@Varuns-MacBook-Air.local>
2025-03-27 14:50:32 -0400

    POC inbox page

---
+++ app/inbox/inbox.tsx
@@ -0,0 +1,98 @@
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { useNavigate } from 'react-router';
+import type { InboxRoomData } from '../service/types';
+import { getRooms } from '../service/chat-service';
+import { getApp } from 'firebase/app';
+
+const Inbox: React.FC = () => {
+  const auth = getAuth(getApp());
+  const navigate = useNavigate();
+  const [rooms, setRooms] = useState<InboxRoomData[]>([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState<string | null>(null);
+
+  const handleRoomClick = (roomId: string) => {
+    // Placeholder
+  };
+
+  // useEffect(() => {
+  //   const fetchRooms = async () => {
+  //     const currentUser = auth.currentUser;
+  //     console.log('currentUser:', currentUser);
+
+  //     if (!currentUser) {
+  //       navigate("/login");
+  //       return;
+  //     }
+
+  //     try {
+  //       const idToken = await currentUser.getIdToken();
+  //       const rooms = await getRooms(idToken);
+  //       setRooms(rooms);
+  //       setError(null);
+  //     } catch (error) {
+  //       console.error("Error getting rooms:", error);
+  //       setError("Failed to get rooms");
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
+
+  //   fetchRooms();
+  // }, [auth, navigate]);
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, async (user) => {
+      if (!user) {
+        navigate("/login");
+        return;
+      }
+
+      try {
+        const idToken = await user.getIdToken();
+        const rooms = await getRooms(idToken);
+        setRooms(rooms);
+        setError(null);
+      } catch (error) {
+        console.error("Error getting rooms:", error);
+        setError("Failed to get rooms");
+      } finally {
+        setLoading(false);
+      }
+    });
+
+    return () => unsubscribe();
+  }, [auth, navigate]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
+  if (!rooms.length) {
+    return <div>No rooms found</div>;
+  }
+
+  return (
+    <div>
+        {rooms.map((room) => (
+            <div 
+                key={room.rid} 
+                onClick={() => handleRoomClick(room.rid)} 
+                style={{ cursor: 'pointer', padding: '10px', border: '1px solid #ccc', marginBottom: '10px' }}
+            >
+                <h3>{room.listingTitle}</h3>
+                <p>Seller: {room.seller}</p>
+                <p>Buyer: {room.buyer}</p>
+                <p>Last Message: {room.recentMessage}</p>
+            </div>
+        ))}
+    </div>
+  );
+}
+
+export default Inbox;
