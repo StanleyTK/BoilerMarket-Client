2eee525b22d73f2e31c0a3428a124e3722c47d05
Daniel <dxgeva@gmail.com>
2025-03-28 13:20:58 -0400

    Merged functionality for search page queries

--- app/service/fetch-listings.ts
+++ app/service/fetch-listings.ts
@@ -1,70 +1,24 @@
-export async function basicFetchAllListings(idToken: string) {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/listing/get/`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${idToken}`,
-        },
-    });
+export async function fetchAllListings(idToken: string, sortBy: string, sortDirection: "asc" | "desc", keyword?: string) {
+    const body: any = {
+        sort: sortBy,
+        dir: sortDirection,
+    };
 
-    if (!response.ok) {
-        throw new Error('Failed to fetch listings');
-    }
+    if (keyword) {
+        body.keyword = keyword;
+    };
 
-    return response.json();
-}
-
-export async function fetchAllListings(idToken: string, sortBy: string, sortDirection: "asc" | "desc") {
     const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/listing/get/`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
             'Authorization': `Bearer ${idToken}`,
         },
-        body: JSON.stringify({
-            sort: sortBy,
-            dir: sortDirection,
-        }),
+        body: JSON.stringify(body),
     });
 
     if (!response.ok) {
         throw new Error('Failed to fetch listings');
-    }
-
-    return response.json();
-}
-
-export async function basicFetchListingByKeyword(keyword: string, idToken: string) {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/listing/get/${keyword}`, {
-        method: 'GET',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${idToken}`,
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch listings with keyword: ' + keyword);
-    }
-
-    return response.json();
-}
-
-export async function fetchListingByKeyword(keyword: string, idToken: string, sortBy: string, sortDirection: "asc" | "desc") {
-    const response = await fetch(`${import.meta.env.VITE_BASE_URL}/api/listing/get/${keyword}`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-            'Authorization': `Bearer ${idToken}`,
-            body: JSON.stringify({
-                sort: sortBy,
-                dir: sortDirection,
-            }),
-        },
-    });
-
-    if (!response.ok) {
-        throw new Error('Failed to fetch listings with keyword: ' + keyword);
     }
 
     return response.json();
