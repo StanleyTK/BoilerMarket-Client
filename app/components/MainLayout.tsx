4f5f6805985e201efebc4780374599c1aeb4e5df
Daniel <dxgeva@gmail.com>
2025-03-28 14:00:21 -0400

    Set up basic filtering infrastructure

--- app/components/MainLayout.tsx
+++ app/components/MainLayout.tsx
@@ -17,6 +17,14 @@ interface SearchContextProps {
   setSortBy: React.Dispatch<React.SetStateAction<string>>;
   sortDirection: "asc" | "desc";
   setSortDirection: React.Dispatch<React.SetStateAction<"asc" | "desc">>;
+  categoryFilter: string;
+  setCategoryFilter: React.Dispatch<React.SetStateAction<string>>;
+  priceFilter: string;
+  setPriceFilter: React.Dispatch<React.SetStateAction<string>>;
+  locationFilter: string;
+  setLocationFilter: React.Dispatch<React.SetStateAction<string>>;
+  dateFilter: string;
+  setDateFilter: React.Dispatch<React.SetStateAction<string>>;
 }
 
 const SearchContext = createContext<SearchContextProps | undefined>(undefined);
@@ -35,6 +43,10 @@ const MainLayout: React.FC = () => {
   const [listings, setListings] = useState<any[]>([]);
   const [sortBy, setSortBy] = useState('dateListed');
   const [sortDirection, setSortDirection] = useState<'asc' | 'desc'>('desc');
+  const [categoryFilter, setCategoryFilter] = useState('');
+  const [dateFilter, setDateFilter] = useState('');
+  const [priceFilter, setPriceFilter] = useState('');
+  const [locationFilter, setLocationFilter] = useState('');
 
   const handleSearch = async () => {
     const auth = getAuth(getApp());
@@ -55,7 +67,25 @@ const MainLayout: React.FC = () => {
 
   return (
     <ThemeProvider>
-      <SearchContext.Provider value={{ searchQuery, setSearchQuery, listings, setListings, handleSearch, sortBy, setSortBy, sortDirection, setSortDirection }}>
+      <SearchContext.Provider value={{
+        searchQuery,
+        setSearchQuery,
+        listings,
+        setListings,
+        handleSearch,
+        sortBy,
+        setSortBy,
+        sortDirection,
+        setSortDirection,
+        categoryFilter,
+        setCategoryFilter,
+        priceFilter,
+        setPriceFilter,
+        dateFilter,
+        setDateFilter,
+        locationFilter,
+        setLocationFilter
+      }}>
         <div className="min-h-screen bg-gray-100 dark:bg-gray-800 text-black dark:text-white">
           <Navbar />
           <Footer />
