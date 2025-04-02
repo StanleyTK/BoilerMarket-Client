5c42a39e80de69592f9e142b8911dec3bace303a
Andrew Bradley <andrewcbradley007@gmail.com>
2025-04-02 14:50:13 -0400

    Listing page

--- app/service/listing-service.ts
+++ app/service/listing-service.ts
@@ -82,3 +82,25 @@ export async function updateListing(
   }
   return response.json();
 }
+
+
+export async function getListing(
+  lid: number
+){
+
+    const response = await fetch(
+        `${import.meta.env.VITE_BASE_URL}/api/listing/getListing/${lid}`,
+        {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        }
+    );
+
+    if (!response.ok) {
+        throw new Error("Listing not found");
+    }
+
+    return response.json();
+}
